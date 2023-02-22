dec2b76a2631c54ebd80fc963e32e4057d2e58e0
DimiyaJr <dimanrandisa@gmail.com>
2023-02-22 20:52:40 +0530

    add more customization

--- frontend/src/components/Navigation/NavBar.js
+++ frontend/src/components/Navigation/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 import "./navBar.css"
 
 export default function NavBar() {
@@ -8,16 +9,16 @@ export default function NavBar() {
     <div className="navBarBox">
         <div className="navBarLeft">
             <span className='logo'>Hire Vision</span>
+            <PersonPinIcon className='personlogo'/>
         </div>
         <div className="navBarCenter">
-
         </div>
         <div className="navBarRight">
             <div className="navBarLinks">
                 <Stack direction="row" spacing={2}>
-                    <Button variant='PostaJob' href='#contained-buttons'>+ Post a job</Button>
-                    <Button variant='Candidate' href='#contained-buttons'>Candidate Sign-in</Button>
-                    <Button variant='Employer' href='#contained-buttons'>Employer Sign-in</Button>
+                    <Button className='bt1' variant='PostaJob' href='#contained-buttons'>+ Post a job</Button>
+                    <Button className='bt2' variant='Candidate' href='#contained-buttons'>Candidate Sign-in</Button>
+                    <Button className='bt3' variant='Employer' href='#contained-buttons'>Employer Sign-in</Button>
                 </Stack>
             </div>
         </div>
