8a809bbd8d4a0c859145240fd43c56cbbe8974b8
DimiyaJr <dimanrandisa@gmail.com>
2023-02-22 20:30:12 +0530

    Navigation Bar almost done.

--- frontend/src/components/Navigation/NavBar.js
+++ frontend/src/components/Navigation/NavBar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import "./navBar.css"
 
 export default function NavBar() {
   return (
@@ -7,9 +10,17 @@ export default function NavBar() {
             <span className='logo'>Hire Vision</span>
         </div>
         <div className="navBarCenter">
-            
+
         </div>
-        <div className="navBarRight"></div>
+        <div className="navBarRight">
+            <div className="navBarLinks">
+                <Stack direction="row" spacing={2}>
+                    <Button variant='PostaJob' href='#contained-buttons'>+ Post a job</Button>
+                    <Button variant='Candidate' href='#contained-buttons'>Candidate Sign-in</Button>
+                    <Button variant='Employer' href='#contained-buttons'>Employer Sign-in</Button>
+                </Stack>
+            </div>
+        </div>
     </div>
   )
 }
