40caf382a02ef4c93dabdff30d25ebceeac4fc9d
DimiyaJr <dimanrandisa@gmail.com>
2023-02-22 18:55:08 +0530

    Add Material icons package for Customization.

--- frontend/src/components/Navigation/NavBar.js
+++ frontend/src/components/Navigation/NavBar.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function NavBar() {
   return (
     <div className="navBarBox">
-        
+        <div className="navBarLeft">
+            <span className='logo'>Hire Vision</span>
+        </div>
+        <div className="navBarCenter">
+            
+        </div>
+        <div className="navBarRight"></div>
     </div>
   )
 }
