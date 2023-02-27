d373447e4fb71b5ed62f742da854423ebf8dc019
suwickramanayaka <suwickramanayaka@gmail.com>
2023-02-27 09:42:26 +0530

    Change rightpane

--- frontend/src/components/Rightpane/rightpane.js
+++ frontend/src/components/Rightpane/rightpane.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import "./rightpane.css"
+import React from "react";
+import "./rightpane.css";
 
 export default function rightpane() {
-  return (
-    <div className='RightpaneBox'>rightpane</div>
-  )
+  return <div className="RightpaneBox">rightpane</div>;
 }
