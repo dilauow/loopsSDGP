e1188d0fa5632e8b74efccfa1f0294d690de4bad
suwickramanayaka <suwickramanayaka@gmail.com>
2023-02-28 22:51:45 +0530

    did a small change to footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -9,12 +9,13 @@ const Footer = () => {
           <div className="product-title">
             <h2>HireVision</h2>
           </div>
+          <br></br>
           <hr></hr>
           <div className="row">
             {/* Column01 */}
             <div className="col">
-              <ul className="ist-unstyled">
-                <li>Quick Links</li>
+              <ul className="list-unstyled">
+                <h2>Quick Links</h2>
                 <br></br>
                 <li>Home</li>
                 <li>Post a job</li>
@@ -24,8 +25,8 @@ const Footer = () => {
             </div>
             {/* Column02 */}
             <div className="col">
-              <ul className="ist-unstyled">
-                <li>Legal</li>
+              <ul className="list-unstyled">
+                <h2>Legal</h2>
                 <br></br>
                 <li>Terms of Service</li>
                 <li>Privacy Policy</li>
@@ -35,8 +36,12 @@ const Footer = () => {
             {/* Column03 */}
           </div>
         </div>
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} HireVision | All rights reserved.
+          </p>
+        </div>
       </div>
-      <h4>Footer!!!</h4>
     </div>
   );
 };
