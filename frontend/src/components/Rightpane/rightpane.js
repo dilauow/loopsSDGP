483fd73cbc906e49579ed0dd5b44d0d82e035649
suwickramanayaka <suwickramanayaka@gmail.com>
2023-03-04 14:45:44 +0530

    made the job card basic structure

--- frontend/src/components/Rightpane/rightpane.js
+++ frontend/src/components/Rightpane/rightpane.js
@@ -2,7 +2,23 @@ import React from "react";
 import "./rightpane.css";
 
 function Rightpane() {
-  return <div className="RightpaneBox">rightpane</div>;
+  return (
+    <div className="RightpaneBox">
+      rightpane
+      <div className="job-card-container">
+        <div className="job-card">
+          <div className="job-card-first-col">
+            <div className="job-card-icon">ICON</div>
+            <div className="job-card-title-company-name">
+              <h2>JOB Name</h2>
+              <h3>Job company</h3>
+            </div>
+          </div>
+          <div className="job-card-second-col">Button</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Rightpane;
