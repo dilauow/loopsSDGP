c3b4af7f5e0a11a4c29fff0a73e7905ae4ddc253
DimiyaJr <dimanrandisa@gmail.com>
2023-03-21 11:23:19 +0530

    Did some small changes and rout the page for result page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Form from "./pages/Formpage/Form";
 import Frontpage from "./pages/Frontpage/Frontpage";
 import Home from "./pages/Home/Home";
 import Loginpage from "./pages/login/login";
+import Previewpage from "./pages/Previewpage/Preview"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/loginpage" element={<Loginpage />} />
         <Route path="/formpage" element={<Form />} />
         <Route path="/home" element={<Home/>}/>
+        <Route path="/preview" element={<Previewpage/>}/>
       </Routes>
     </div>
   );
--- frontend/src/components/Navigation/NavBar.js
+++ frontend/src/components/Navigation/NavBar.js
@@ -12,6 +12,12 @@ export default function NavBar() {
         navigate("/loginpage")
     }
 
+    const navigate2 = useNavigate();
+
+    function handleclick2(){
+        navigate2("/loginpage")
+    }
+
   return (
     <div className="navBarBox">
         <div className="navBarLeft">
@@ -24,7 +30,7 @@ export default function NavBar() {
             <div className="navBarLinks">
                 <Stack direction="row" spacing={3}>
                     <Button className='bt2' variant='Candidate' onClick={handleclick}>Admin Sign-in</Button>
-                    <Button className='bt3' variant='Employer'>Post a Job</Button>
+                    <Button className='bt3' variant='Employer'onClick={handleclick2}>Post a Job</Button>
                 </Stack>
             </div>
         </div>
