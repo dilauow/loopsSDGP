fbe61b995efe1951121173eb1b1891d183af2bcc
thilini2 <thilini.20200476@iit.ac.lk>
2023-03-21 12:52:37 +0530

    Did some changes for preview page

--- frontend/src/pages/Previewpage/Preview.js
+++ frontend/src/pages/Previewpage/Preview.js
@@ -1,66 +1,35 @@
 import React from 'react'
 import "./preview.css"
+import NavBar from "../../components/Navigation/NavBar"
 
 export default function Preview() {
   return (
     <div>
-      <h1>Final Result</h1>
-      <hr></hr>
-      <div>
-        <div class="col-3">
-          <i class="fa fa-quote-left"></i>
-        </div>
-        <table align='"center' cellpadding="10px" width="600px">
-          <tr>
-            <td>
-              <h3> Personal Details</h3>
-            </td>
-          </tr>
-        </table>
-        <table align='center' cellpadding="10px" width="600px">
-          <tr>
-            <td>Name : </td>
-          </tr>
-          <tr>
-            <td>Age : </td>
-          </tr>
-          <tr>
-            <td>Email :</td>
-          </tr>
-          <tr>
-            <td>Phone number :</td>
-          </tr>
-        </table>
-        <table align="center" cellPadding="10px" width="600px">
-          <tr>
-            <td>
-              <h3> Personality Traits Result</h3>
-            </td>
-          </tr>
-        </table>
-        <table align="center" cellpadding="10px" width="600px">
-	<tr>
-			<td>1 Openness =</td>
-			<td>%</td>
-		</tr>
-		<tr>
-			<td>2 Conscientiousness =</td>
-			<td>%</td>
-		</tr>
-		<tr>
-			<td>3 Extraversion =</td>
-			<td>%</td>
-		</tr>
-		<tr>
-			<td>4 Agreeableness =</td>
-			<td>%</td>
-		</tr>
-		<tr>
-			<td>5 Neuroticism =</td>
-			<td>%</td>
-		</tr>
-    </table>
-      </div>
-    </div> 
-  )
+    <NavBar/>
+    <h1 className='head'>Final Result</h1>
+
+    <hr></hr>
+
+    <div></div>
+
+    <h3 className='head1'> Personal Details</h3>
+
+    <div>
+      <h4 className='head4'> Name = </h4>
+      <h4 className='head4'> Age = </h4>
+      <h4 className='head4'> Email =</h4>
+      <h4 className='head4'> Phone Number =</h4>
+    </div>
+
+    <h3 className='head2'> Personality Traits Result</h3>
+    <hr></hr>
+  <div>
+    <h4 className='head3'> 1 Openness = </h4>
+    <h4 className='head3'> 2 Conscientiousness = </h4>
+    <h4 className='head3'> 3 Extraversion =</h4>
+    <h4 className='head3'> 4 Agreeableness =</h4>
+    <h4 className='head3'> 5 Neuroticism =</h4>
+  </div>
+</div>
+  );
 }
