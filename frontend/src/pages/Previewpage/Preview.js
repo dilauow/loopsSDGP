1100b3aac4b288a19d9af6d4d5dd655a66a5444a
thilini2 <thilini.20200476@iit.ac.lk>
2023-03-21 13:35:05 +0530

    Did some changes in preview page

--- frontend/src/pages/Previewpage/Preview.js
+++ frontend/src/pages/Previewpage/Preview.js
@@ -7,27 +7,37 @@ export default function Preview() {
     <div>
     <NavBar/>
     <h1 className='head'>Final Result</h1>
-
     <hr></hr>
-
-    <div></div>
-
+    <br></br>
+    <div class = "table">
     <h3 className='head1'> Personal Details</h3>
-
-    <div>
+    </div>
+    <br></br>
+    
+    <div class = "details">
       <h4 className='head4'> Name = </h4>
+      <br></br>
       <h4 className='head4'> Age = </h4>
+      <br></br>
       <h4 className='head4'> Email =</h4>
+      <br></br>
       <h4 className='head4'> Phone Number =</h4>
     </div>
 
+    <br></br>
     <h3 className='head2'> Personality Traits Result</h3>
     <hr></hr>
+    <br></br>
   <div>
+    <br></br>
     <h4 className='head3'> 1 Openness = </h4>
+    <br></br>
     <h4 className='head3'> 2 Conscientiousness = </h4>
+    <br></br>
     <h4 className='head3'> 3 Extraversion =</h4>
+    <br></br>
     <h4 className='head3'> 4 Agreeableness =</h4>
+    <br></br>
     <h4 className='head3'> 5 Neuroticism =</h4>
   </div>
 </div>
