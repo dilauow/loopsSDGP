0d9ce137241496b3b7545552614df9c40be07d9a
thilini2 <thilini.20200476@iit.ac.lk>
2023-03-21 22:08:33 +0530

    did some changes in preview.js

--- frontend/src/pages/Previewpage/Preview.js
+++ frontend/src/pages/Previewpage/Preview.js
@@ -4,42 +4,62 @@ import NavBar from "../../components/Navigation/NavBar"
 
 export default function Preview() {
   return (
-    <div>
-    <NavBar/>
-    <h1 className='head'>Final Result</h1>
-    <hr></hr>
-    <br></br>
-    <div class = "table">
-    <h3 className='head1'> Personal Details</h3>
-    </div>
-    <br></br>
-    
-    <div class = "details">
-      <h4 className='head4'> Name = </h4>
-      <br></br>
-      <h4 className='head4'> Age = </h4>
-      <br></br>
-      <h4 className='head4'> Email =</h4>
-      <br></br>
-      <h4 className='head4'> Phone Number =</h4>
-    </div>
+    <div className="mainbody">
+      <h1 className="title" align="center">Final Result</h1>
+      <hr className="divider" />
+      <div className="row">
+        <div className="col-3">
+          <i className="fa fa-quote-left"></i>
+        </div>
+      </div>
 
-    <br></br>
-    <h3 className='head2'> Personality Traits Result</h3>
-    <hr></hr>
-    <br></br>
-  <div>
-    <br></br>
-    <h4 className='head3'> 1 Openness = </h4>
-    <br></br>
-    <h4 className='head3'> 2 Conscientiousness = </h4>
-    <br></br>
-    <h4 className='head3'> 3 Extraversion =</h4>
-    <br></br>
-    <h4 className='head3'> 4 Agreeableness =</h4>
-    <br></br>
-    <h4 className='head3'> 5 Neuroticism =</h4>
-  </div>
-</div>
+      <table align="center" cellpadding="10px" width="600px">
+        <tr>
+          <td> <h3> Personal Details </h3> </td>
+        </tr>
+      </table>
+      <table align="center" cellpadding="10px" width="600px">
+        <tr>
+          <td>Name :</td>
+        </tr>
+        <tr>
+          <td>Age :</td>
+        </tr>
+        <tr>
+          <td>Email :</td>
+        </tr>
+        <tr>
+          <td>Phone number :</td>
+        </tr>
+      </table>
+
+      <table align="center" cellpadding="10px" width="600px">
+        <tr>
+          <td> <h3> Personality Traits Result </h3> </td>
+        </tr>
+      </table>
+      <table align="center" cellpadding="10px" width="600px">
+        <tr>
+          <td>1 Openness =</td>
+          <td>%</td>
+        </tr>
+        <tr>
+          <td>2 Conscientiousness =</td>
+          <td>%</td>
+        </tr>
+        <tr>
+          <td>3 Extraversion =</td>
+          <td>%</td>
+        </tr>
+        <tr>
+          <td>4 Agreeableness =</td>
+          <td>%</td>
+        </tr>
+        <tr>
+          <td>5 Neuroticism =</td>
+          <td>%</td>
+        </tr>
+      </table>
+    </div>
   );
 }
