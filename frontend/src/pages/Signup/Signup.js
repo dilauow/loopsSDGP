a4263671c584701f4c50559d9fbb6ca267a93bd3
Sahasri Kalutota <126221606+SahasriKalutota@users.noreply.github.com>
2023-03-22 15:22:31 +0530

    signup page done

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -13,49 +13,52 @@ function Signup() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        
-        <input
-          type="text"
-          id="name"
-          placeholder='Name'
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <div>
-        
-        <input
-          type="email"
-          id="email"
-          placeholder='Email'
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <div>
-        
-        <input
-          type="password"
-          id="password"
-          placeholder='Password'
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div>
-        
-        <input
-          type="password"
-          id="confirmPassword"
-          placeholder='Confirm Password'
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        />
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className='bg'>
+      <form onSubmit={handleSubmit}>
+        <h1>SignUp to Hire Vision</h1>
+        <div>
+          
+          <input
+            type="text"
+            id="name"
+            placeholder='Name'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          
+          <input
+            type="email"
+            id="email"
+            placeholder='Email'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div>
+          
+          <input
+            type="password"
+            id="password"
+            placeholder='Password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder='Confirm Password'
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
   );
 }
 
