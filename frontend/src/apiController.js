4f34e3c4bbd3a04c11dffa80a3c9720cbaf56b4e
dilauow <dilshara.20210305@iit.ac.lk>
2023-03-30 15:46:42 +0530

    added headers

--- frontend/src/apiController.js
+++ frontend/src/apiController.js
@@ -4,10 +4,21 @@ const api = axios.create({
   baseURL: "http://localhost:8000/api",
 });
 
+const headers = {
+    'Content-Type': 'application/json',
+  }
+  
+
+// const config = {
+//     headers:{
+//       header1: value1,
+//       header2: value2
+//     }
+//   };
 // CREATE APPLICATI
 export const createApplication = async (application) => {
     try {
-        const res = await api.post("/application", application);
+        const res = await api.post("/application", application,headers);
         return res.data;
     } catch (error) {
         console.error(error);   
