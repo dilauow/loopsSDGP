e07ed0f197b8572ecab15a5a57c1c121a4b89237
DimiyaJr <dimanrandisa@gmail.com>
2023-04-03 19:36:27 +0530

    Did some changes . (CSS, Add privacy 7 terms pages )

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import './Footer.css';
+import { useNavigate } from 'react-router-dom';
 
 function Footer() {
+  const navigate = useNavigate();
+
+  function handleclick() {
+    navigate("/Tos")
+  }
+
+  function handleclick1() {
+    navigate("/")
+  }
+
+  function handleclick2() {
+    navigate("/prs")
+  }
+
   return (
     <footer className="footer">
       <div className="footer-container">
-        
-        <h1>HireVision</h1>
+
+        <h1 onClick={handleclick1}>HireVision</h1>
         <div className="footer-links">
-          
+
           <div className='list'>
             <ul className="footer-links-column">
               <li>Home</li>
               <li>Post a Job</li>
-              <li>Candidate Sign In</li>
             </ul>
           </div>
           <div className='list'>
             <ul className="footer-links-column">
               <li>Employer Sign In</li>
-              <li>Terms of Service</li>
-              <li>Privacy Policy</li>
+              <li onClick={handleclick}>Terms of Service</li>
+              <li onClick={handleclick2}>Privacy Policy</li>
               <li>GDPR Fields</li>
             </ul>
           </div>
---
+++ frontend/src/pages/TermsOfServices/termsofservice.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Termofservice.css'
+// import { motion } from "framer-motion";
+import Footer from '../../components/Footer/Footer'
+
+function termsofservice() {
+  return (
+    <div className="Apps" initial={{width:0}} animate={{width:"100%"}} exit={{x:"100%"}}>
+      <header className="App-header">
+        <h1>Terms of Service</h1>
+      </header>
+      <main>
+        <p>
+          This website is provided as a service to our customers. Please read
+          these Terms of Service carefully as they govern your use of the
+          website.
+        </p>
+        <h2>Acceptance of Terms</h2>
+        <p>
+          By accessing or using the website, you agree to be bound by these
+          Terms of Service. If you do not agree to these terms, you may not use
+          the website.
+        </p>
+        <h2>Disclaimer</h2>
+        <p>
+          The content of this website is for informational purposes only and is
+          not intended to be a substitute for professional advice, diagnosis, or
+          treatment. Always seek the advice of your physician or other qualified
+          healthcare provider with any questions you may have regarding a medical
+          condition.
+        </p>
+        <h2>Limitation of Liability</h2>
+        <p>
+          In no event shall the website or its owners be liable for any damages
+          whatsoever arising out of the use of or inability to use the website,
+          even if the website or its owners have been advised of the possibility
+          of such damages.
+        </p>
+        <h2>Modification of Terms</h2>
+        <p>
+          These Terms of Service may be modified at any time without notice. By
+          using the website after any modifications have been made, you agree to
+          be bound by the modified terms.
+        </p>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions or concerns regarding these Terms of Service,
+          please contact us at hirevision@gmail.com.
+        </p>
+      </main>
+      <div className='footer1'>
+        <Footer/>
+      </div>
+    </div>
+  )
+}
+
+export default termsofservice
