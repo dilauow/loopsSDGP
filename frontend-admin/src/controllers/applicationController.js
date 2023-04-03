94c0048364f76b0e88b036f186fda1be6f02515c
dilauow <dilshara.20210305@iit.ac.lk>
2023-04-03 15:15:57 +0530

    added deployed APi

--- frontend-admin/src/controllers/applicationController.js
+++ frontend-admin/src/controllers/applicationController.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://fastapi-production-8423.up.railway.app/api",
 });
 
 const headers = {
